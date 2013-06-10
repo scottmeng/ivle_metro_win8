96eae05e604505d286718a759c0806cb08204757
Scott Meng <blackridermkz@gmail.com>
2013-06-11 00:59:05 +0800

    adding utilities classes, log in function halfly done

--- icreate-test2/icreate-test2/MainPage.xaml.cs
+++ icreate-test2/icreate-test2/MainPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Text;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -11,6 +13,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
+using System.Net.NetworkInformation;
 
 // The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
 
@@ -21,6 +25,11 @@ namespace icreate_test2
     /// </summary>
     public sealed partial class MainPage : icreate_test2.Common.LayoutAwarePage
     {
+        private String username;
+        private String password;
+        private String domain;
+        private String postString;
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -48,15 +57,15 @@ namespace icreate_test2
             Windows.Storage.ApplicationDataContainer roamingSettings = Windows.Storage.ApplicationData.Current.RoamingSettings;
             if (roamingSettings.Values.ContainsKey("userID"))
             {
-                userID.Text = roamingSettings.Values["userID"].ToString();
+                UsernameTextBox.Text = roamingSettings.Values["userID"].ToString();
             }
             if (roamingSettings.Values.ContainsKey("password"))
             {
-                password.Password = roamingSettings.Values["password"].ToString();
+                PasswordBox.Password = roamingSettings.Values["password"].ToString();
             }
             if (roamingSettings.Values.ContainsKey("domain"))
             {
-                domain.SelectedItem = roamingSettings.Values["domain"].ToString();
+                DomainComboBox.SelectedItem = roamingSettings.Values["domain"].ToString();
             }
         }
         /// <summary>
@@ -69,27 +78,133 @@ namespace icreate_test2
         {
         }
 
-        private void login_Click(object sender, RoutedEventArgs e)
+        private async void login_Click(object sender, RoutedEventArgs e)
         {
-            progressRing.IsActive = true;
+            if (!NetworkInterface.GetIsNetworkAvailable())
+            {
+                MessageDialog noInternetDialog = new MessageDialog("There is currently no internet connection..", "Oops");
+                await noInternetDialog.ShowAsync();
+            }
+            else
+            {
+                progressRing.IsActive = true;
+
+                username = UsernameTextBox.Text;
+                password = PasswordBox.Password;
+                domain = DomainComboBox.SelectedValue.ToString();
+
+                postString = Utils.LAPI.GeneratePostString(username, password, domain);
+
+                string authenticationURL = "https://ivle.nus.edu.sg/api/Lapi.svc/Login_JSON";
+
+                // Create a new HttpWebRequest object.
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(authenticationURL);
+
+                // Set the Method property to 'POST' to post data to the URI.
+                request.Method = "POST";
+
+                request.ContentType = "application/x-www-form-urlencoded";
+
+                request.BeginGetRequestStream(new AsyncCallback(GetRequestStreamCallback), request);
+            }
+
         }
+
+        public void GetRequestStreamCallback(IAsyncResult asynchronousResult)
+        {
+            HttpWebRequest request = (HttpWebRequest)asynchronousResult.AsyncState;
+
+            Stream postStream = request.EndGetRequestStream(asynchronousResult);
+
+            byte[] byteArray = Encoding.UTF8.GetBytes(postString);
+
+            postStream.Write(byteArray, 0, postString.Length);
+            postStream.Dispose();
+
+            request.BeginGetResponse(new AsyncCallback(GetResponseCallback), request);
+        }
+
+        private void GetResponseCallback(IAsyncResult asynchronousResult)
+        {
+            HttpWebRequest request = (HttpWebRequest)asynchronousResult.AsyncState;
+            HttpWebResponse response;
+
+            try
+            {
+                response = (HttpWebResponse)request.EndGetResponse(asynchronousResult);
+                Stream streamResponse = response.GetResponseStream();
+                StreamReader streamRead = new StreamReader(streamResponse);
+
+                // returned JSON string for validation
+                string responseString = streamRead.ReadToEnd();
+
+                // remove the last "}"
+                responseString = responseString.Remove(responseString.Length - 1);
+
+                // remove the first "{" and its associated header
+                responseString = responseString.Substring(responseString.IndexOf(":") + 1);
+                Token token = (Token)Deserialize(responseString, typeof(Token));
+
+                if (token != null && token.TokenSuccess.Equals(true))
+                {
+                    LAPI.token = token.TokenContent;
+
+                    Dispatcher.BeginInvoke(() =>
+                    {
+                        (Application.Current as App).online = true;
+
+                        SaveCredentials();
+                        loginProgressBar.IsIndeterminate = false;
+                        NavigationService.Navigate(new Uri(("/MenuPage.xaml"), UriKind.Relative));
+                    });
+                }
+                else
+                {
+                    Dispatcher.BeginInvoke(() =>
+                    {
+                        loginProgressBar.IsIndeterminate = false;
+
+                        MessageBox.Show("Log in failed");
+                    });
+                }
+
+                // Close the stream object
+                streamResponse.Close();
+                streamRead.Close();
+
+                // Release the HttpWebResponse
+                response.Close();
+            }
+            catch (WebException ex)
+            {
+                if (ex.Status == WebExceptionStatus.RequestCanceled)
+                {
+                    Dispatcher.BeginInvoke(() =>
+                    {
+                        Login();
+                    });
+                }
+            }
+        }
+
+
         private void userIDChanged(object sender, TextChangedEventArgs e)
         {
             Windows.Storage.ApplicationDataContainer roamingSettings = Windows.Storage.ApplicationData.Current.RoamingSettings;
-            roamingSettings.Values["userID"] = userID.Text;
+            roamingSettings.Values["userID"] = UsernameTextBox.Text;
             
         }
 
         private void passwordChanged(object sender, RoutedEventArgs e)
         {
             Windows.Storage.ApplicationDataContainer roamingSettings = Windows.Storage.ApplicationData.Current.RoamingSettings;
-            roamingSettings.Values["password"] = password.Password;
+            roamingSettings.Values["password"] = PasswordBox.Password;
         }
 
         private void domainSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             Windows.Storage.ApplicationDataContainer roamingSettings = Windows.Storage.ApplicationData.Current.RoamingSettings;
-            roamingSettings.Values["domain"] = domain.SelectedItem;
+            roamingSettings.Values["domain"] = DomainComboBox.SelectedItem;
         }
     }
 }
---
+++ icreate-test2/icreate-test2/Utils/LAPI.cs
@@ -0,0 +1,51 @@
+﻿// LAPI.cs
+// Store domain URL, token and API key
+// for National University of Singapore
+// IVLE services
+// Created by Kaizhi Meng on 10 June 2013
+// Last edited on 10 June 2013
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace icreate_test2.Utils
+{
+    class LAPI
+    {
+        private static String key = "lAY3TAAcAGYcokEEqKNCt";
+        private static String domain = "https://ivle.nus.edu.sg/";
+        public static String token { get; set; }
+
+        // get the request url with API item and parameters
+        public static string GenerateURL(string item, Dictionary<String,String> parameters)
+        {
+            string url = domain + "api/Lapi.svc/" + item + "?APIKey=" + key + "&AuthToken=" + token;
+
+            foreach (KeyValuePair<String, String> parameter in parameters)
+            {
+                url += "&" + parameter.Key+ "=" + parameter.Value;
+            }
+
+            return url;
+        }
+
+        // return the url for token achieve
+        public static string GeneratePostString(string userid, string password, string domain)
+        {
+            string url = "APIKey=" + key + "&UserID=" + userid + "&Password=" + password + "&Domain=" + domain;
+            
+            return url;
+        }
+
+        // generate the url for downloading files
+        public static string GenerateDownloadURL(string id)
+        {
+            string url = domain + "api/downloadfile.ashx?APIKey=" + key + "&AuthToken=" + token + "&ID=" + id + "&target=workbin";
+
+            return url;
+        }
+    }
+}
