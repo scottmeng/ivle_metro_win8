40dc7d16521b3345723a75e710bde09bd8bf1fd9
Scott Meng <blackridermkz@gmail.com>
2013-07-06 01:12:11 +0800

    add examinfo data structure, add async exam info data retrieval, minor changes on item page loading

---
+++ icreate-test2/icreate-test2/DataStructure/ExamInfoWrapper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace icreate_test2.DataStructure
+{
+    [DataContract]
+    class ExamInfoWrapper
+    {
+        [DataMember(Name = "Results")]
+        public ExamInfo[] examInfos { get; set; }
+
+        [DataMember(Name = "Comments")]
+        public string comments { get; set; }
+
+        public ExamInfoWrapper(ExamInfo[] infos, string myComments)
+        {
+            examInfos = infos;
+            comments = myComments;
+        }
+    }
+}
