89cee70c973c1b0f43ad5a5142d68834183f6827
Scott Meng <blackridermkz@gmail.com>
2013-07-22 02:02:52 +0800

    add thread wrapper class

---
+++ icreate-test2/icreate-test2/DataStructure/ThreadWrapper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace icreate_test2.DataStructure
+{
+    [DataContract]
+    class ThreadWrapper
+    {
+        [DataMember(Name = "Results")]
+        public Thread[] threads { get; set; }
+
+        [DataMember(Name = "Comments")]
+        public string comments { get; set; }
+
+        public ThreadWrapper(Thread[] ts, String cs)
+        {
+            threads = ts;
+            comments = cs;
+        }
+    }
+}
